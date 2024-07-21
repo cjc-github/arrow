7689464ccc75a00768aea362c5cef75c50cd8fb6
cjc <49013706+cjc-github@users.noreply.github.com>
2024-07-21 22:33:14 +0800

    update

--- arrow/_version.py
+++ arrow/_version.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-# arrow版本
-__version__ = "1.3.0"
-=======
 # arrow版本, 修改为Alpha版本
 __version__ = "1.3.0.Alpha"
->>>>>>> e5b2cc271b4f7ed6bceffbcfee7c5732021e4973
--- usage/main.py
+++ usage/main.py
@@ -35,8 +35,35 @@ wheel                 0.38.4
 """
 
 if __name__ == "__main__":
+    # 打印版本
     print(arrow.__version__)
 
+    # 调用arrow/factory.py文件中的now函数
+    """
+    函數原型: now(tz: Optional[TZ_EXPR] = None) -> Arrow
+    返回一个：class: `Arrow <Arrow.Arrow.Arrow>`对象，表示给定时区中的当前时间。
+    ：param tz:(可选) A: ref: =`时区表达式 <tz-expr>`。默认为当地时间。
+
+    用法：
+    >>> import arrow
+    >>> arrow.now()
+    2024-07-21T22:07:04.908242+08:00
+    >>> arrow.now('US/Pacific')
+    2024-07-21T07:07:04.972242-07:00
+    >>> arrow.now("+02:00")
+    2024-07-21T16:07:04.972242+02:00
+    >>> arrow.now("local")
+    2024-07-21T22:07:04.972242+08:00
+    """
     print(arrow.now())
+    print(arrow.now('US/Pacific'))
+    print(arrow.now("+02:00"))
+    print(arrow.now("local"))
+
     print(arrow.get())
-    print(arrow.utcnow())
+
+    # 调用arrow/arrow.py文件中的utcnow函数
+    x = arrow.utcnow()
+    print(x.year, x.month, x.day, x.hour, x.minute, x.second, x.microsecond, x.tzinfo, x.fold)
+    print(x.float_timestamp, x.int_timestamp, x.naive)
+    print(x, x.datatime)
